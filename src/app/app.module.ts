c4a4cf21753bea607c36778d9ed1e8c8c42450e7
Alex <aquinobean@gmail.com>
2021-02-02 08:28:14 +0900

    add auth guard and auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthGuard } from './auth.guard';
+import { AuthenticationService } from './service/authentication.service';
+import { AuthenticationInterceptor } from './interceptor/authentication.interceptor';
+
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,21 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (as: AuthenticationService) => {
+        return () => {
+           return as.load(); 
+        }
+      },
+      deps:[AuthenticationService],
+      multi: true
+    },
+    httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/interceptor/authentication.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+} from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../service/authentication.service';
+
+/** Add token parameter */
+@Injectable()
+export class AuthenticationInterceptor implements HttpInterceptor {
+
+    constructor(private auth: AuthenticationService){}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      if(!this.auth.isAuthenticated())
+        return next.handle(req);
+      const authReq = req.clone({
+          headers: req.headers.set("Authorization", "Bearer " + this.auth.accessToken)
+      })
+      return next.handle(authReq);
+  }
+
+}
