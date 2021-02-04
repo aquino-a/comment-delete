937eb223d96e3c9bc2f916c4d13b26248318cede
Alex <aquinobean@gmail.com>
2021-02-04 13:14:35 +0900

    change auth to use backend

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -12,22 +12,13 @@ import { ok } from 'assert';
 })
 export class AuthenticationService {
   
-  private clientId = environment.clientId;
-  private clientSecret = environment.clientSecret;
-  private redirect = environment.redirect;
-
-  private tokenUrl = "https://www.reddit.com/api/v1/access_token";
+  private tokenUrl = environment.tokenUrl;
   private meUrl = "https://oauth.reddit.com/api/v1/me";
+
   accessToken: string;
   currentUser: User;
 
-  // private userLoginSuccessSource = new BehaviorSubject<User>(null);
-  // private userLoginFailSource = new Subject<any>();
   
-  // userLoginSuccess$ = this.userLoginSuccessSource.asObservable();
-  // userLoginFail$ = this.userLoginFailSource.asObservable();
-
-
   constructor(private http: HttpClient, private router: Router) {
      
   }
@@ -45,28 +36,22 @@ export class AuthenticationService {
       this.accessToken = localStorage.token;
       this.currentUser = JSON.parse(localStorage.user) as User;
       resolve(null);
-      // this.setUser(this.accessToken).subscribe(u => resolve(null), error => { this.clearToken(); resolve(null); });
     }).catch(error => console.log(error));
   }
 
 
   authenticate(code: string) {
-    const body = `grant_type=authorization_code&code=${code}&redirect_uri=${this.redirect}`;
-
-    this.http.post<TokenResponse>(this.tokenUrl, body,
-      {
-        headers: new HttpHeaders()
-            .set('Authorization', 'Basic ' + btoa(this.clientId + ':' + this.clientSecret))
-            .set('Content-Type', 'application/x-www-form-urlencoded')
-      })
-      .subscribe(tr =>{
-        this.accessToken = tr.access_token;
-        localStorage.token = tr.access_token;
-        this.setUser(this.accessToken);
-      }, error => {
-        console.log(error); 
-        this.clearToken();
-      });
+    this.http.get<TokenResponse>(this.tokenUrl, {
+      params: new HttpParams()
+                  .set("code", code)
+    }).subscribe(tr =>{
+      this.accessToken = tr.access_token;
+      localStorage.token = tr.access_token;
+      this.setUser(this.accessToken);
+    }, error => {
+      console.log(error); 
+      this.clearToken();
+    });
   }
 
 
