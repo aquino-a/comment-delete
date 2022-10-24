d0c7ceda8d7905d95530304a41a957ceb3fdea18
alex <29205589+aquino-a@users.noreply.github.com>
2022-10-24 16:55:25 +0900

    use minimal image

--- build-image.sh
+++ build-image.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-npm install
-npm run-script build
+# npm install
+# npm run-script build
 
 source ./set-secrets.sh
 
-microcontainer=$(buildah from docker.io/redhat/ubi8-micro:latest)
+microcontainer=$(buildah from registry.access.redhat.com/ubi8/nodejs-14-minimal:latest)
 micromount=$(buildah mount $microcontainer)
-dnf --installroot $micromount \
-    --nogpgcheck \
-	--setopt=reposdir=/etc/yum.repos.d \
-	install \
-	node \
-	-y
-dnf --installroot $micromount clean all
+
 buildah copy $microcontainer './dist/comment-delete' '/home/comment-delete' 
 buildah copy $microcontainer './server/server.js' '/home/comment-delete/server.js' 
+
+npm install express minimist --prefix $micromount/home/comment-delete
 
 buildah config --env CD_ID=$CD_ID $microcontainer
 buildah config --env CD_SECRET=$CD_SECRET $microcontainer
@@ -22,7 +18,7 @@ buildah config --env CD_PORT=$CD_PORT $microcontainer
 buildah config --env CD_ORIGIN=$CD_ORIGIN $microcontainer
 buildah config --env CD_REDIRECT=$CD_REDIRECT $microcontainer
 
-buildah config --cmd "node server.js --clientId=$CD_ID --clientSecret=$CD_SECRET --port=$CD_PORT --origin=$CD_ORIGIN  --redirect=$CD_REDIRECT --app-folder=/home/comment-delete" $microcontainer
+buildah config --cmd "node /home/comment-delete/server.js --clientId=$CD_ID --clientSecret=$CD_SECRET --port=$CD_PORT --origin=$CD_ORIGIN  --redirect=$CD_REDIRECT --app-folder=/home/comment-delete" $microcontainer
 
 rm -rf $micromount/var/cache $micromount/var/log/*
 
