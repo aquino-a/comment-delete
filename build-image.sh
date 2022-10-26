b0b69d242132f950bdaf47ffcb72fbfcea4b1416
alex <29205589+aquino-a@users.noreply.github.com>
2022-10-26 15:46:19 +0900

    add buildah script

---
+++ build-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+npm install
+npm run-script build
+
+source ./set-secrets.sh
+
+microcontainer=$(buildah from registry.access.redhat.com/ubi8/nodejs-14-minimal:latest)
+micromount=$(buildah mount $microcontainer)
+
+buildah copy $microcontainer './dist/comment-delete' '/home/comment-delete' 
+buildah copy $microcontainer './server/server.js' '/home/comment-delete/server.js' 
+buildah copy $microcontainer './server/package.json' '/home/comment-delete/package.json' 
+
+npm install --prefix $micromount/home/comment-delete
+
+buildah config --env CD_ID=$CD_ID $microcontainer
+buildah config --env CD_SECRET=$CD_SECRET $microcontainer
+buildah config --env CD_PORT=$CD_PORT $microcontainer
+buildah config --env CD_ORIGIN=$CD_ORIGIN $microcontainer
+buildah config --env CD_REDIRECT=$CD_REDIRECT $microcontainer
+
+buildah config --cmd "node /home/comment-delete/server.js --clientId=$CD_ID --clientSecret=$CD_SECRET --port=$CD_PORT --origin=$CD_ORIGIN  --redirect=$CD_REDIRECT --app-folder=/home/comment-delete" $microcontainer
+
+rm -rf $micromount/var/cache $micromount/var/log/*
+
+buildah umount $microcontainer
+buildah rmi comment-delete
+buildah commit $microcontainer comment-delete
+buildah delete $microcontainer
+
+
